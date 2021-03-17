e5e8b7df97d0ee0260c8430ca3140d36391c937f
Dewinsy <mushdaisy71@gmail.com>
2021-03-17 15:56:30 +0300

    loops

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("while loop");
+var logs = 0;
+while (logs < 10) {
+  
+  logs++;
+  console.log(logs);
+}
+console.log("****************************************************");
+
+console.log("for loop");
+for (var nums = 0; nums <= 10; nums++) {
+  
+  console.log(nums);
+}
+console.log("****************************************************");
+
+console.log("do-while loops");
+let trials = 0;
+do{
+    trials++;
+    console.log(trials);
+}
+while(trials<=9);
+console.log("****************************************************");
+
+
